b1600404f3a1118242e7b3876061e0e4541fa5fb
Huxleigh Services <service@huxleigh.com>
2025-10-28 17:32:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9495804365-10cc6",
+  "appId": "1:567050794013:web:c412fc0b2a25669f8e82f4",
+  "apiKey": "AIzaSyD5sRxjZRD-DJ3ymAj61g6SJHrGevdVspA",
+  "authDomain": "studio-9495804365-10cc6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "567050794013"
+};
