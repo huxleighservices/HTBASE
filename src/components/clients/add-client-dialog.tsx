e6a627a9c6f35f00e73dc07a487d66fc4f6abefc
Huxleigh Services <service@huxleigh.com>
2025-10-28 15:51:52 +0000

    Window is too long here when adding a new client. Make it two pages plea

--- src/components/clients/add-client-dialog.tsx
+++ src/components/clients/add-client-dialog.tsx
@@ -11,7 +11,6 @@ import {
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { useForm, type SubmitHandler } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
@@ -49,163 +48,212 @@ export function AddClientDialog({
   onAddClient,
 }: AddClientDialogProps) {
   const [isOpen, setIsOpen] = useState(false);
+  const [step, setStep] = useState(1);
+
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-        firmName: '',
-        legalFirstName: '',
-        legalLastName: '',
-        firmSize: '',
-        firmEstYear: '',
-        industry: '',
-        contactEmail: '',
-        contactPhoneNumber: '',
-        location: '',
+      firmName: '',
+      legalFirstName: '',
+      legalLastName: '',
+      firmSize: '',
+      firmEstYear: '',
+      industry: '',
+      contactEmail: '',
+      contactPhoneNumber: '',
+      location: '',
     },
   });
 
+  const handleNext = async () => {
+    const fieldsToValidate: (keyof FormValues)[] = [
+      'firmName',
+      'legalFirstName',
+      'legalLastName',
+      'firmSize',
+      'firmEstYear',
+    ];
+    const isValid = await form.trigger(fieldsToValidate);
+    if (isValid) {
+      setStep(2);
+    }
+  };
+  
+  const handleOpenChange = (open: boolean) => {
+    if (!open) {
+      form.reset();
+      setStep(1);
+    }
+    setIsOpen(open);
+  }
+
   const onSubmit: SubmitHandler<FormValues> = data => {
     onAddClient(data);
-    form.reset();
-    setIsOpen(false);
+    handleOpenChange(false);
   };
 
   return (
-    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+    <Dialog open={isOpen} onOpenChange={handleOpenChange}>
       <div onClick={() => setIsOpen(true)}>{children}</div>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>Add New Client</DialogTitle>
           <DialogDescription>
-            Enter the client information below. Click add when you're done.
+            Enter the client information below. Step {step} of 2.
           </DialogDescription>
         </DialogHeader>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-            <FormField
-              control={form.control}
-              name="firmName"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Firm or Representative Name</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-             <FormField
-              control={form.control}
-              name="legalFirstName"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Legal First Name</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-             <FormField
-              control={form.control}
-              name="legalLastName"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Legal Last Name</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="firmSize"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Firm Size</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="firmEstYear"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Firm Est. Year</FormLabel>
-                  <FormControl>
-                    <Input type="number" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="industry"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Industry</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="contactEmail"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Contact Email</FormLabel>
-                  <FormControl>
-                    <Input type="email" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="contactPhoneNumber"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Contact Phone Number</FormLabel>
-                  <FormControl>
-                    <Input type="tel" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-             <FormField
-              control={form.control}
-              name="location"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Location</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+            {step === 1 && (
+              <>
+                <FormField
+                  control={form.control}
+                  name="firmName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Firm or Representative Name</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="legalFirstName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Legal First Name</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="legalLastName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Legal Last Name</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="firmSize"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Firm Size</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="firmEstYear"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Firm Est. Year</FormLabel>
+                      <FormControl>
+                        <Input type="number" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </>
+            )}
+
+            {step === 2 && (
+              <>
+                <FormField
+                  control={form.control}
+                  name="industry"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Industry</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="contactEmail"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Contact Email</FormLabel>
+                      <FormControl>
+                        <Input type="email" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="contactPhoneNumber"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Contact Phone Number</FormLabel>
+                      <FormControl>
+                        <Input type="tel" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="location"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Location</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </>
+            )}
+
             <DialogFooter>
               <DialogClose asChild>
                 <Button type="button" variant="secondary">
                   Cancel
                 </Button>
               </DialogClose>
-              <Button type="submit">Add Client</Button>
+              {step === 1 && (
+                <Button type="button" onClick={handleNext}>
+                  Next
+                </Button>
+              )}
+              {step === 2 && (
+                <>
+                  <Button
+                    type="button"
+                    variant="secondary"
+                    onClick={() => setStep(1)}
+                  >
+                    Back
+                  </Button>
+                  <Button type="submit">Add Client</Button>
+                </>
+              )}
             </DialogFooter>
           </form>
         </Form>
