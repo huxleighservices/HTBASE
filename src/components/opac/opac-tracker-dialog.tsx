555a9453cca93ed2ec9ec255fb7b32a1b020cb0f
Huxleigh Services <service@huxleigh.com>
2025-11-24 16:30:42 +0000

    the scrolling on the customer list pane is still not working

--- src/components/opac/opac-tracker-dialog.tsx
+++ src/components/opac/opac-tracker-dialog.tsx
@@ -147,7 +147,7 @@ export function OpacTrackerDialog({ open, onOpenChange, client, activeUserDispla
                 A tool to keep track of certain customers for {client.firmName}.
             </DialogDescription>
         </DialogHeader>
-        <div className="flex flex-col gap-8 pt-4 h-full">
+        <div className="flex flex-col gap-8 pt-4 flex-grow min-h-0">
             <div className="flex justify-between items-center">
                 <div />
                  <Button onClick={() => setIsAddCustomerOpen(true)}>
